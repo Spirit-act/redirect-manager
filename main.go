21e1ab06d2279741658a59bcd4919da6c803a8a8
Spirit-act <16631523+Spirit-act@users.noreply.github.com>
2023-12-18 02:21:57 +0100

    add documentation
    add helper function

--- main.go
+++ main.go
@@ -12,17 +12,39 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+// create an error object if a no redirect is found (404)
 var ErrNotFound = errors.New(toString(http.StatusNotFound))
 
+// create an logging for default logging -> stdout
 var stdout = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+// create logging for errors -> stderr
 var stderr = slog.New(slog.NewJSONHandler(os.Stderr, nil))
 
 var ctx = context.Background()
 
+// convert any value to a string
 func toString(value any) string {
 	return fmt.Sprintf("%v", value)
 }
 
+// convert a string to an integer
+// it only supports string because Atoi only supports string
+// if you want other types use the following syntax
+// -> toInt(toString(errors.Error()))
+func toInt(value string) int {
+	val, err := strconv.Atoi(value)
+
+	if err != nil {
+		// if an error occurs: panic
+		panic(err)
+	}
+
+	return val
+}
+
+// retrieve the value from an env variable
+// if the key does not exist, return an fallback
 func getEnv(key string, fallback string) string {
 	val, exists := os.LookupEnv(key)
 
@@ -33,23 +55,28 @@ func getEnv(key string, fallback string) string {
 	return fallback
 }
 
+// retrieve a value from redis
 func getRedis(host_header string) (string, error) {
-	redis_db, _ := strconv.Atoi(getEnv("REDIS_DB", "0"))
-
+	// connect to the readis server
 	rdb := redis.NewClient(&redis.Options{
 		Addr:     getEnv("REDIS_HOST", "127.0.0.1:6379"),
 		Username: getEnv("REDIS_USERNAME", "default"),
 		Password: getEnv("REDIS_PASSWORD", ""),
-		DB:       redis_db,
+		DB:       toInt(getEnv("REDIS_DB", "0")),
 	})
 
+	// retrieve a value by key
 	val, err := rdb.Get(ctx, host_header).Result()
 
 	if err == redis.Nil {
+		// if redis returns nil the Key was not found
+		// so we return an custom NotFound Error
 		return "", ErrNotFound
 	}
 
 	if err != nil {
+		// if some other error occures
+		// return a normal error
 		return "", err
 	}
 
@@ -57,26 +84,34 @@ func getRedis(host_header string) (string, error) {
 }
 
 func handleRequest(w http.ResponseWriter, req *http.Request) {
+	// get the value based on the request Host Header
 	val, err := getRedis(req.Host)
 
 	if err == nil {
+		// Log a successfull request to stdout
 		stdout.Info("redirect",
 			"status", http.StatusMovedPermanently,
 			"host", req.Host,
 			"target", val)
+		// redirect with 301 Moved Permanently
 		http.Redirect(w, req, val, http.StatusMovedPermanently)
 		return
 	}
 
 	if errors.Is(err, ErrNotFound) {
+		// if the custom NotFound Error is returned
+		// Log the Request and return 404
 		stdout.Info("not found",
 			"status", http.StatusNotFound,
 			"host", req.Host)
+		// return 404 Not Found
 		http.NotFound(w, req)
 		return
 	}
 
+	// if everything else went wrong log an error to stderr
 	stderr.Error(err.Error(), "status", http.StatusInternalServerError, "host", req.Host)
+	// and return 500 Internal Server Error
 	http.Error(w, err.Error(), http.StatusInternalServerError)
 }
 
